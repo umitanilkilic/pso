65b72914444be3b2a69ddb66609468f95c816c95
umit <umitanilkilic@gmail.com>
2024-06-06 03:13:25 +0300

    performance improment

--- pso.go
+++ pso.go
@@ -1,7 +1,7 @@
 package pso
 
 type PSO struct {
-	Swarm            *Swarm
+	swarm            *Swarm
 	iteration        int
 	currentIteration int
 }
@@ -9,8 +9,8 @@ type PSO struct {
 // runner is a function that executed after each iteration
 type runner func(swarm *Swarm)
 
-func NewPSO(swarm Swarm) PSO {
-	return PSO{Swarm: &swarm}
+func NewPSO(sw Swarm) PSO {
+	return PSO{swarm: &sw}
 }
 
 func (pso *PSO) SetIterationCount(iteration int) {
@@ -22,23 +22,26 @@ func (pso *PSO) GetCurrentIteration() int {
 }
 
 func (pso *PSO) GetSwarm() *Swarm {
-	return pso.Swarm
+	return pso.swarm
 }
 
 func (pso *PSO) performNextIteration() {
-	pso.Swarm.CalculateVelocity()
-	pso.Swarm.UpdatePosition()
-	pso.Swarm.UpdateBestPosition()
-	pso.Swarm.UpdateGlobalBest()
+	for i := 0; i < len(pso.swarm.Particles); i++ {
+		particle := &pso.swarm.Particles[i]
+		pso.swarm.CalculateVelocity(particle)
+		pso.swarm.UpdateBestPositions(particle)
+	}
 }
 
 func (pso *PSO) Optimize(executeFunc ...runner) {
-	pso.Swarm.UpdateBestPosition()
-	pso.Swarm.UpdateGlobalBest()
+	for i := 0; i < len(pso.swarm.Particles); i++ {
+		particle := &pso.swarm.Particles[i]
+		pso.swarm.UpdateBestPositions(particle)
+	}
 	for pso.currentIteration = 0; pso.currentIteration < pso.iteration; pso.currentIteration++ {
 		pso.performNextIteration()
 		if len(executeFunc) > 0 {
-			executeFunc[0](pso.Swarm)
+			executeFunc[0](pso.swarm)
 		}
 	}
 }
